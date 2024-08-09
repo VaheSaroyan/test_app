4a2e85b014441a07bc83a4d129d6578df29e75e2
Vahe Saroyan <vahe.saroyan@codsbe.com>
2024-08-10 02:18:45 +0400

    developed(main_animation): wave animations realization - realised animation with hook usage - Closes: #6

--- App.tsx
+++ App.tsx
@@ -1,18 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+import Animated from 'react-native-reanimated';
+
+import { useWaveAnimation } from '~/hooks/useWaveAnimation';
 
 export default function App() {
+  const { animatedStyle, startAnimation, previousBackgroundColor } = useWaveAnimation();
+
   return (
-    <View style={styles.container}>
-      <Text>Hello there</Text>
-    </View>
+    <TouchableOpacity onPress={startAnimation} style={styles.root} activeOpacity={1}>
+      <Animated.View style={[styles.container, { backgroundColor: previousBackgroundColor }]}>
+        <Animated.View style={[styles.wave, animatedStyle]} />
+        <Text style={styles.text}>Hello there</Text>
+      </Animated.View>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    position: 'relative'
+  },
+  text: {
+    fontSize: 57,
+    color: '#ffffff',
+    fontWeight: 'bold'
+  },
+  wave: {
+    position: 'absolute',
+    borderRadius: 1000
   }
 });
